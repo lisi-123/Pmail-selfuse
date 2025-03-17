5726c91fe574ffe6eb9104d46e6d501d78b7a409
lisi-123 <160303845+lisi-123@users.noreply.github.com>
2025-03-17 11:35:05 +0800

    Create start-pmail.sh

---
+++ start-pmail.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# 赋予可执行权限
+chmod +x /root/Pmail-selfuse/pmail_linux_amd64
+cd /root/Pmail-selfuse
+
+# 让用户输入端口号
+read -p "请输入端口号: " PORT
+
+# 修改 delete-pmail.sh 和 script.sh 中的端口号
+sed -i "s/pmail_linux_amd64 -p [0-9]\+/pmail_linux_amd64 -p $PORT/" delete-pmail.sh
+sed -i "s/pmail_linux_amd64 -p [0-9]\+/pmail_linux_amd64 -p $PORT/" script.sh
+
+# 运行程序
+./pmail_linux_amd64 -p "$PORT"
